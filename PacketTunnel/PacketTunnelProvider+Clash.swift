bce39626336905496f8f947abdfc808416300887
Arror <maqiang@autoai.com>
2022-03-04 14:39:21 +0800

    Set native logger

--- PacketTunnel/PacketTunnelProvider+Clash.swift
+++ PacketTunnel/PacketTunnelProvider+Clash.swift
@@ -2,19 +2,20 @@ import Foundation
 import CommonKit
 import ClashKit
 
-extension PacketTunnelProvider: ClashPacketFlowProtocol, ClashTrafficReceiverProtocol {
+extension PacketTunnelProvider: ClashPacketFlowProtocol, ClashTrafficReceiverProtocol, ClashNativeLoggerProtocol {
     
     func setupClash() throws {
         let config = """
         mixed-port: 8080
         mode: \(UserDefaults.shared.string(forKey: Constant.tunnelMode) ?? ClashTunnelMode.rule.rawValue)
-        log-level: silent
+        log-level: info
         """
         var error: NSError? = nil
         ClashSetup(self, Constant.homeDirectoryURL.path, config, &error)
         if let error = error {
             throw error
         }
+        ClashSetNativeLogger(self)
         ClashSetTrafficReceiver(self)
     }
     
@@ -37,5 +38,12 @@ extension PacketTunnelProvider: ClashPacketFlowProtocol, ClashTrafficReceiverPro
     func receiveTraffic(_ up: Int64, down: Int64) {
         UserDefaults.shared.set(Double(up), forKey: ClashTraffic.up.rawValue)
         UserDefaults.shared.set(Double(down), forKey: ClashTraffic.down.rawValue)
+    }
+    
+    func log(_ level: String?, payload: String?) {
+        guard let level = level, let payload = payload else {
+            return
+        }
+        NSLog("---> \(level): \(payload)")
     }
 }
