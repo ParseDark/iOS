3613122b6038851bdfb60e97292ba1c0f665658d
Arror <maqiang@autoai.com>
2022-03-09 11:24:12 +0800

    Remove unused code

--- CommonKit/ClashLogLevel.swift
+++ CommonKit/ClashLogLevel.swift
@@ -1,27 +1,8 @@
 import Foundation
 
-public enum ClashLogLevel: String, Identifiable, CaseIterable, Comparable {
+public enum ClashLogLevel: String, Identifiable, CaseIterable {
         
     public var id: Self { self }
     
     case silent, info, debug, warning, error
-    
-    public static func < (lhs: ClashLogLevel, rhs: ClashLogLevel) -> Bool {
-        lhs.weight < rhs.weight
-    }
-    
-    private var weight: Int {
-        switch self {
-        case .silent:
-            return 0
-        case .info:
-            return 1
-        case .debug:
-            return 2
-        case .warning:
-            return 3
-        case .error:
-            return 4
-        }
-    }
 }
